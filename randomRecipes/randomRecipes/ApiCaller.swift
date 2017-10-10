a671d26a046b4488f357713d923b942ad9276782
Jareedos <jaredallens@gmail.com>
2017-10-09 19:08:37 -0700

    fixed Double bang Bug

--- randomRecipes/randomRecipes/ApiCaller.swift
+++ randomRecipes/randomRecipes/ApiCaller.swift
@@ -19,17 +19,29 @@ class ApiCaller {
                 do {
                 //Decode retrived data with JSONDecoder and assing type of Article object
 //                let recipes = try JSONDecoder().decode([Article].self, from: data)
-                    let json = try? JSONSerialization.jsonObject(with: usableData, options: .allowFragments) as! [String:AnyObject]
-                    print(json)
-                //Get back to the main queue
-//                DispatchQueue.main.async {
-//                    //print(articlesData)
-////                    self.articles = articlesData
-////                    self.collectionView?.reloadData()
-//                }
-                
-                } catch  {
-//                print(jsonError)
+                    let json = try JSONSerialization.jsonObject(with: usableData, options: .allowFragments) as! [String:AnyObject]
+//                    print(json)
+                    let meals = json["meals"] as! [[String:String?]]
+//                    print(meals)
+                    let recipeOrigin = meals[0]["strArea"]
+                    let recipeType = meals[0]["strCategory"]
+                    let recipeTitle = meals[0]["strMeal"]
+                    let recipeImage = meals[0]["strMealThumb"]
+                    let recipeInstructions = meals[0]["strInstructions"]
+                    var tupleArray: [(String, String)] = []
+                    for index in 1...20 {
+                        let tuple = (meals[0]["strIngredient\(index)"], meals[0]["strMeasure\(index)"])
+                        guard let cleanString = tuple.0 else {return}
+                        if cleanString != "" && cleanString != "null"{
+                            tupleArray.append((tuple.0!!,tuple.1!!))
+                        }
+                    }
+                    print(tupleArray)
+                    let newRecipe = Recipe(recipeTitle: recipeTitle!!, recipeType: recipeType!!, recipeOrigin: recipeOrigin!!, recipeImage: recipeImage!!, recipeInstructions: recipeInstructions!!)
+                    newRecipe._ingredients.append(contentsOf: tupleArray)
+                    print(newRecipe)
+                } catch {
+                    fatalError("Sorry")
                 }
              }
         }.resume()
--- randomRecipes/randomRecipes/Recipe.swift
+++ randomRecipes/randomRecipes/Recipe.swift
@@ -13,13 +13,15 @@ class Recipe {
     let _recipeType: String
     let _recipeOrigin: String
     let _recipeImage: String
+    let _recipeInstructions: String
     var _ingredients: [(String,String)] = [] // creating an array of tuples for the ingredients and it's measurements
     
-    init(recipeTitle: String, recipeType: String, recipeOrigin: String, recipeImage: String){
+    init(recipeTitle: String, recipeType: String, recipeOrigin: String, recipeImage: String, recipeInstructions: String){
         _recipeTitle = recipeTitle
         _recipeType = recipeType
         _recipeOrigin = recipeOrigin
         _recipeImage = recipeImage
+        _recipeInstructions = recipeInstructions
     }
     
 }
