b496dc4218ed34be07d7020c7fd18a418f287c50
Jareedos <jaredallens@gmail.com>
2017-10-16 12:15:04 -0700

    Improved the look of the Image & text on RecipeDetailVC

--- randomRecipes/RecipeDetailVC.swift
+++ randomRecipes/RecipeDetailVC.swift
@@ -11,10 +11,29 @@ import CoreData
 
 class RecipeDetailVC: UIViewController {
     var recipeObj: NSManagedObject!
-
+    @IBOutlet weak var recipeTitle: UILabel!
+    @IBOutlet weak var recipeType: UILabel!
+    @IBOutlet weak var recipeImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        if let title = recipeObj.value(forKey: "recipeTitle") as? String {
+            recipeTitle.text = title
+        }
+        
+        if let Image = recipeObj.value(forKey: "recipeImage") as? Data {
+            let image = UIImage(data: Image)
+            // SDWebImage
+            recipeImage.image = image
+            recipeImage.layer.cornerRadius = 10
+            recipeImage.layer.masksToBounds = true
+        }
+        
+        if let type = recipeObj.value(forKey: "recipeType") as? String {
+            recipeType.text = type
+        }
+        
+       
         // Do any additional setup after loading the view.
     }
 
@@ -22,4 +41,5 @@ class RecipeDetailVC: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 }
